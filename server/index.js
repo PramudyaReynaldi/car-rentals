13bb49e2d862302a502dc989523276bb9eadb206
PramudyaReynaldi <prmdy02@gmail.com>
2023-12-26 01:12:05 +0700

    update authentication authorize[server]🚀

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,7 @@ try {
     console.log(error);
 }
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { getUsers, registerUser } from '../controller/Users.js';
+import { getUsers, registerUser, loginUser } from '../controller/Users.js';
+import { verifyToken } from '../middleware/VerifyToken.js';
+import { refreshToken } from '../controller/RefreshToken.js';
 
 const router = express.Router();
 
-router.get('/users', getUsers);
+router.get('/users', verifyToken, getUsers);
 router.post('/users', registerUser);
+router.post('/login', loginUser);
+router.post('/token', refreshToken);
 
 export default router;
