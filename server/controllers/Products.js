34dc7704cf68a61f4e2f3bbdc6a33279cadcd9e5
PramudyaReynaldi <prmdy02@gmail.com>
2024-01-07 23:24:54 +0700

    server💻: add logic for list cars

--- server/controllers/Products.js
+++ server/controllers/Products.js
@@ -1,118 +1,141 @@
 import Products  from "../models/ProductModel.js";
-import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-// export const getProducts = async (req, res) => {
-//     try {
-//         console.log('Start getProducts');
-
-//         const apiResponse = await axios.get(process.env.API_BASE_URL);
-//         console.log('API Response:', apiResponse.data);
-
-//         const apiData = apiResponse.data;
-
-//         // Menyimpan data dari API ke tabel Products
-//         const savedProducts = await Products.bulkCreate(apiData.map(item => ({
-//             uuid: item.uuid,
-//             plate: item.plate,
-//             manufacture: item.manufacture,
-//             model: item.model,
-//             image: item.image,
-//             rentPerDay: item.rentPerDay,
-//             capacity: item.capacity,
-//             description: item.description,
-//             availableAt: item.availableAt,
-//         })));
-
-//         console.log('Saved Products:', savedProducts);
-
-//         // Mengambil data dari tabel Products
-//         const dbData = await Products.findAll({
-//             attributes: ['uuid', 'plate', 'manufacture', 'model', 'image', 'rentPerDay', 'capacity', 'description'],
-//         });
-
-//         console.log('DB Data:', dbData);
-
-//         res.status(200).json({ apiData, dbData });
-//     } catch (error) {
-//         console.error('Error:', error.message);
-//         res.status(500).json({ msg: error.message });
-//     }
-// };
 
 export const getProducts = async (req, res) => {
     try {
         console.log('Start getProducts');
 
-        const apiResponse = await axios.get(process.env.API_BASE_URL);
-        console.log('API Response:', apiResponse.data);
-
-        const apiData = apiResponse.data;
-
-        // Ekstrak UUID dari data API untuk memeriksa keberadaan data di database
-        const apiUuids = apiData.map(item => item.uuid);
-
-        // Mengambil data yang sudah ada di database
-        const existingProducts = await Products.findAll({
-            where: { uuid: apiUuids },
-            attributes: ['uuid'],
-        });
-
-        // Menyaring data yang belum ada di database
-        const newData = apiData.filter(item => !existingProducts.some(existing => existing.uuid === item.uuid));
-
-        // Menyimpan data baru ke dalam database
-        const savedProducts = await Products.bulkCreate(newData);
-
-        console.log('Saved Products:', savedProducts);
-
-        // Mengambil data dari tabel Products
-        const dbData = await Products.findAll({
+        const carsData = await Products.findAll({
             attributes: ['uuid', 'plate', 'manufacture', 'model', 'image', 'rentPerDay', 'capacity', 'description'],
         });
 
-        console.log('DB Data:', dbData);
+        console.log('Cars Data:', carsData);
 
-        res.status(200).json({ apiData, dbData });
+        res.status(200).json({ carsData });
     } catch (error) {
         console.error('Error:', error.message);
         res.status(500).json({ msg: error.message });
     }
 };
+
 export const getProductById = async (req, res) => {
-}
+    try {
+        const product = await Products.findOne({
+            attributes: ['uuid', 'plate', 'manufacture', 'model', 'image', 'rentPerDay', 'capacity', 'description'],
+            where: {
+                uuid: req.params.id
+            }
+        });
+        if(!product) return res.status(404).json({ msg: 'Product not found' });
+
+        res.status(200).json({ product });
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ msg: error.message });
+    }
+};
+  
 
 export const createProduct = async (req, res) => {
-    
-}
+    try {
+        const {
+            uuid,
+            plate,
+            manufacture,
+            model,
+            image,
+            rentPerDay,
+            capacity,
+            description,
+            availableAt,
+            transmission,
+            available,
+            type,
+            year
+        } = req.body;
+
+        const newProduct = await Products.create({
+            uuid: uuid,
+            plate: plate,
+            manufacture: manufacture,
+            model: model,
+            image: image,
+            rentPerDay: rentPerDay,
+            capacity: capacity,
+            description: description,
+            availableAt: availableAt,
+            transmission: transmission,
+            available: available,
+            type: type,
+            year: year,
+        });
+        console.log('New Product:', newProduct);
+        res.status(201).json({ newProduct });
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ msg: error.message });
+    }
+};
 
 export const updateProduct = async (req, res) => {
-    
-}
+    const product = await Products.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    if(!product) return res.status(404).json({msg: "Product not found"});
+
+    const {
+        plate,
+        manufacture,
+        model,
+        image,
+        rentPerDay,
+        capacity,
+        description,
+        availableAt,
+        transmission,
+        available,
+        type,
+        year
+    } = req.body;
+
+    try {
+        await Products.update({
+            plate: plate,
+            manufacture: manufacture,
+            model: model,
+            image: image,
+            rentPerDay: rentPerDay,
+            capacity: capacity,
+            description: description,
+            availableAt: availableAt,
+            transmission: transmission,
+            available: available,
+            type: type,
+            year: year,
+        }, {
+            where: {
+                id: product.id
+            }
+        });
+        res.status(200).json({msg: "Update Success"});
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ msg: error.message });
+    }
+};
+
 
 export const deleteProduct = async (req, res) => {
     try {
         const { id } = req.params;
+        if (!id) return res.status(400).json({ msg: 'Invalid UUID parameter' });
 
-        // Periksa apakah uuid tidak didefinisikan atau tidak valid
-        if (!id) {
-            return res.status(400).json({ msg: 'Invalid UUID parameter' });
-        }
-
-        // Periksa apakah produk dengan UUID tertentu ada di database
         const existingProduct = await Products.findOne({ where: { id } });
+        if (!existingProduct) return res.status(404).json({ msg: 'Product not found' });
 
-        if (!existingProduct) {
-            // Jika tidak ditemukan, kirim respons bahwa produk tidak ditemukan
-            return res.status(404).json({ msg: 'Product not found' });
-        }
-
-        // Hapus produk berdasarkan UUID
         await Products.destroy({ where: { id } });
 
-        // Kirim respons bahwa produk berhasil dihapus
         res.status(200).json({ msg: 'Product deleted successfully' });
     } catch (error) {
         console.error('Error:', error.message);
